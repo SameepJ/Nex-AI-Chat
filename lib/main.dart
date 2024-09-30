429bc550e28ecba49d3ab4756355184f1ce3d41b
Sameep Jain <sameepjain13@gmail.com>
2024-09-30 21:49:56 +0530

    Nex-Ai chat

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
+      theme:ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
